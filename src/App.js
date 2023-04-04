d07ad5b3871620767679c92d485947c201194da2
Analise <analise.giobbi@gc.com>
2023-04-04 14:52:08 -0400

    main pages set

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
-import PortfolioContainer from ''
+import PortfolioContainer from './components/PortfolioContainer'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
-const App = () => <PortfolioContainer />;
+function App() {
+    return(
+        <div>
+            <PortfolioContainer />
+            <Header />
+            <Footer />
+        </div>
+    )
+}
 
 export default App;
